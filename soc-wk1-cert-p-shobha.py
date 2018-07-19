6066d66abc6b1ad5450eb93c3fa362ad49fab031
pshobha97 <36978736+pshobha97@users.noreply.github.com>
2018-07-19 22:48:52 +0530

    Create soc-wk1-cert-p-shobha.py

---
+++ soc-wk1-cert-p-shobha.py
@@ -0,0 +1,39 @@
+how many hours in a year?
+`int hour,day,hours_in_year;
+string year;
+hour=24;
+
+enter the year as leapyear or noleapyear
+
+year=input('enter the year');
+if(year==leapyear)
+{
+day=366;
+}
+else if(year==noleapyear)
+{
+day=365;
+}
+else
+print("enter leapyear or noleapyear");
+
+hours_in_year=day*hour;
+print("hour in the year is"+hours_in_year);
+
+minutes in decade
+int minute=1036524*60;
+print("minutes in decade is"+minute);
+
+my age in seconds
+int age_in_second;
+age_in_second=20365246060;
+print("my age in second is"+age);
+
+Calculate @Andreea Visanoiu's age.
+
+int age;
+age= 48618000 /(3652460*60);
+print("age of Andreea Visanoiu is "+age);
+
+#612
+`
